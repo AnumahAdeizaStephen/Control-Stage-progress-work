228919b0f19ae37dbbd596ed6fea4e5fa9fde35b
Adeiza Anumah <Localadmin_adeizaan@lh4-500-31318.ad.helsinki.fi>
2026-02-20 00:14:36 +0200

    fix: reorder import statements for clarity in aigrid_gui.py

--- aigrid_gui.py
+++ aigrid_gui.py
@@ -1,8 +1,6 @@
 # aigrid_gui.py
 # Python 2.7 (inside IDEAS)
 
-from aigrid_client import StageClient
-import tb_product
 from fileinput import filename
 import sys
 import Tkinter as tk
@@ -11,6 +9,8 @@ import os
 sys.path.append(
     r"C:\Users\Localadmin_adeizaan\Desktop\Test_bench\IDEASTestbench_V1_6_4_1\scripts\GDS-100")
 
+from aigrid_client import StageClient
+import tb_product
 
 # Ensure sys.argv exists for IDEAS
 if not hasattr(sys, 'argv'):
